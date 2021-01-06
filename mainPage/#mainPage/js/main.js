e89a8a5d072d93acfad40d51728af1942da52e96
Dmitry <zombak1234@yandex.ru>
2021-01-06 11:16:00 +0300

    q

--- mainPage/#mainPage/js/main.js
+++ mainPage/#mainPage/js/main.js
@@ -1,2 +1,19 @@
 @@include("swiperSettings.js");
-@@include("_FaQ.js")
+@@include("_FaQ.js");
+@@include("#header.js");
+
+/* slow scroll */
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
