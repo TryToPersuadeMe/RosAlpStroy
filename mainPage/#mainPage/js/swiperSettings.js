50f4f233b8e512a42077271620e23a0114bc5eb4
Dmitry <zombak1234@yandex.ru>
2021-01-07 02:16:09 +0300

    responsive

--- mainPage/#mainPage/js/swiperSettings.js
+++ mainPage/#mainPage/js/swiperSettings.js
@@ -8,23 +8,21 @@ new Swiper(".sliderGallery__slider", {
     nextEl: ".sliderGallery__button-next",
     prevEl: ".sliderGallery__button-prev",
   },
+  grabCursor: true,
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: true,
+    loadPrevNext: true,
+  },
 
-  /* Количество слайдов для показа */
-  /* адаптив по высоте */
-  // autoHeight:"auto"
-  /* адаптив */
-  // watchOverflow: true,
-  /* отсутпы между слайдами */
+  watchSlidesProgress: false,
+  watchSlidesVisibility: false,
+
   spaceBetween: 50,
-  /* центрирование слайда*/
-
-  /* стартовый слайд */
-  // initialSlide: 1,
-  /* Количество пролистываемых слайдов */
   breakpoints: {
     319: {
       slidesPerGroup: 1,
-  spaceBetween: 50,
+      spaceBetween: 50,
       slidesPerView: 1,
     },
     769: {
@@ -38,53 +36,6 @@ new Swiper(".sliderGallery__slider", {
       slidesPerView: 3,
     },
   },
-  /* несколько рядом в слайдере */
-  // slidesPerColumn: 1,
-  /* скорость прокуртки */
+
   speed: 800,
-  /* вертикальный слайдер */
-  // direction: "vertical",
-  /* эффекты переключения */
-
-  // effect: "coverflow",
-  // coverflowEffect: {
-  //   // угол
-  //   rotate: 20,
-  //   stretch: 50,
-  //   slidesShadows: true,
-  // },
-
-  // effect: "fade",
-  // effect: "fade",
-  // fadeEffect: {
-  // смена прозрачности
-  // crossFade: true,
-  // },
-  // effect: "flip",
-  // flipEffect: {
-  /* тень */
-  //   slidesShadows: true,
-  //   limitRotation: true,
-  // },
-  // effect: "cube",
-  // cubeEffect: {
-  /* тень */
-  // slidesShadows: true,
-  // shadow: true,
-  // shadowEffect: 20,
-  // shadowScale: 0.94,
-  //   limitRotation: true,
-  // },
-  /* бесконечная прокрутка */
-  // loop:true
-  // loopSize:3
-  /* автопрокрутка */
-  // autoplay:{
-  /* время прокуртки */
-  // delay:1000,
-  /* закончить напоследнем слайде */
-  // stopOnLastSslide:true,
-  /* отключитьпосле ручного переключения */
-  // disableOnInteracion:false
-  // }
 });
